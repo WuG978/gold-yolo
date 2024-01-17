4d9c0469ea9e299ddd29b0b45104441da9f4d31b
xux_ubuntu_server <xux@xux.com>
2024-01-17 20:29:06 +0800

    for training

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from tools.train import get_args_parser, main
 
 if __name__=="__main__":
     args = get_args_parser().parse_args()
-    args.batch_size = 64
-    print(args)
-    # main(args)
+    # args.batch_size = 64
+    # print(args)
+    main(args)
